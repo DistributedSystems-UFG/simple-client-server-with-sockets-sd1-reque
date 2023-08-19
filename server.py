001c80f8c5f23209e07bb49f6f17b78b268ae8bb
IanChaves <45765511+IanChaves@users.noreply.github.com>
2023-08-19 14:54:41 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,36 @@
-from socket  import *
-from constCS import * #-
+from socket import *
+from constCS import *
 
-s = socket(AF_INET, SOCK_STREAM) 
-s.bind((HOST, PORT))  #-
-s.listen(1)           #-
-(conn, addr) = s.accept()  # returns new socket and addr. client 
-while True:                # forever
-  data = conn.recv(1024)   # receive data from client
-  if not data: break       # stop if client stopped
-  print(bytes.decode(data))
-  conn.send(str.encode(bytes.decode(data)+"*")) # return sent data plus an "*"
-conn.close()               # close the connection
+def handle_request(request):
+    parts = request.split()
+    if len(parts) != 3:
+        return "Invalid request"
+    
+    operation = parts[0]
+    num1 = int(parts[1])
+    num2 = int(parts[2])
+
+    if operation == "ADD":
+        return str(num1 + num2)
+    elif operation == "SUB":
+        return str(num1 - num2)
+    else:
+        return "Unknown operation"
+
+s = socket(AF_INET, SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+
+print("Server is listening...")
+(conn, addr) = s.accept()
+
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+
+    request = bytes.decode(data)
+    result = handle_request(request)
+    conn.send(str.encode(result))
+
+conn.close()
