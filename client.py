5ed5dd9e1a8dcda4003e03c1780f022da48e2003
IanChaves <45765511+IanChaves@users.noreply.github.com>
2023-08-19 14:55:02 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,24 @@
-from socket  import *
-from constCS import * #-
+from socket import *
+from constCS import *
+
+def send_request(s, request):
+    s.send(str.encode(request))
+    data = s.recv(1024)
+    return bytes.decode(data)
 
 s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('Hello, world'))  # send some data
-data = s.recv(1024)     # receive the response
-print (bytes.decode(data))            # print the result
-s.close()               # close the connection
+s.connect((HOST, PORT))
+
+while True:
+    operation = input("Enter operation (ADD/SUB): ")
+    if operation == "exit":
+        break
+
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+
+    request = f"{operation} {num1} {num2}"
+    result = send_request(s, request)
+    print("Result:", result)
+
+s.close()
